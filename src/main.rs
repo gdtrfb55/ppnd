8bdf1df2d18f483b677de620229f45f890aa6e65
Jack Browning <gdtrfb55@gmail.com>
2021-10-24 15:11:44 -0500

    Changed variable name for clarity.

--- src/main.rs
+++ src/main.rs
@@ -43,9 +43,9 @@ mod ifregex {
     use regex::Regex;
 
     pub fn build() -> Result<Regex, String> {
-        const IF_LINE_RE: &str = r"^\s*[[:alnum:]]+:(?:\s+\d+){16}$";
+        const INTERFACE_LINE: &str = r"^\s*[[:alnum:]]+:(?:\s+\d+){16}$";
 
-        if let Ok(r) = Regex::new(IF_LINE_RE) { return Ok(r) };
+        if let Ok(r) = Regex::new(INTERFACE_LINE) { return Ok(r) };
         Err("regular expression is invalid".to_string())
     }
 }
